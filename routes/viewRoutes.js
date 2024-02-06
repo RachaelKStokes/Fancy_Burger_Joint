d5411737565ae3bc5086f320d3fcd16be981e18f
unknown <rachael.kate.stokes@gmail.com>
2024-02-05 19:22:44 -0500

    updated folders, added db folder with schema file, added login.js and logout.js to the public folder - this fixed an error we had in the console

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,16 +1,33 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  res.render('index');
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
 });
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
-});
-
 module.exports = router;
+
