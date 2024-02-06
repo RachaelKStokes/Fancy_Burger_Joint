9debb579acc72023b2b957ede89d61b69f2ea560
Sarahbryant411 <sarahbryant411@gmail.com>
2024-02-05 21:05:30 -0500

    added the tables for schema, possibly need to do more tweaking

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,36 @@
 DROP DATABASE IF EXISTS user_db;
 CREATE DATABASE user_db;
+
+USE user_db;
+
+CREATE TABLE restaurant (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  location VARCHAR(100),
+  phone VARCHAR(100),
+  email VARCHAR(100),
+  password VARCHAR(100)
+);
+
+CREATE TABLE customer (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100),
+  password VARCHAR(100)
+);
+
+CREATE TABLE event (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+    date DATE,
+    time TIME,
+    location VARCHAR(100),
+);
+
+CREATE TABLE reservation (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT,
+  event_id INT,
+  FOREIGN KEY (customer_id) REFERENCES customer(id),
+  FOREIGN KEY (event_id) REFERENCES event(id)
+);
