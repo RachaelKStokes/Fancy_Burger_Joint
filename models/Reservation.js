f6bebbc328454f5294a549b43fa159361017ba21
Zach <zach.Brewer.1995@gmail.com>
2024-02-07 19:57:33 -0500

    added reservation controller and model

---
+++ models/Reservation.js
@@ -0,0 +1,39 @@
+const { model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Reservation extends model {}
+
+Reservation.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    reservation_date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+
+    reservation_time: {
+      type: DataTypes.TIME,
+      allowNull: false
+    },
+
+    reservation_guests: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'reservation',
+  }
+);
+
+module.exports = Reservation;
