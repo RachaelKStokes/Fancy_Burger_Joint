ec2d2573ed8095e65003e89ef2d2b6c6099ce983
Sarahbryant411 <sarahbryant411@gmail.com>
2024-02-07 20:29:14 -0500

    added files

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,53 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const withAuth = require('../../utils/auth');
 
-
-// GET all users
-router.get('/', async (req, res) => {
-    try {
-        const userData = await User.findAll({
-            attributes: { exclude: ['password'] }
-        });
-        res.json(userData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// GET a single user
-router.get('/:id', async (req, res) => {
-    try {
-        const userData = await User.findByPk(req.params.id, {
-            attributes: { exclude: ['password'] }
-        });
-
-        if (!userData) {
-            res.status(404).json({ message: 'No user found with that id!' });
-            return;
-        }
-
-        res.json(userData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// POST a new user
-router.post('/', async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.loggedIn = true;
-            res.status(200).json(userData);
-        });
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
-
-// PUT (update) a user
 router.post('/login', async (req, res) => {
     try {
         const userData = await User.findOne({ where: { email: req.body.email } });
@@ -80,7 +33,6 @@ router.post('/login', async (req, res) => {
       }
 });
 
-// DELETE a user
 router.post('/logout', (req, res) => {
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const eventRoutes = require('./eventRoutes');
+const userRoutes = require('./api/userRoutes');
+const eventRoutes = require('./api/eventRoutes');
 const apiRoutes = require('./api');
 const viewRoutes = require('./viewRoutes');
+const menuRoutes = require('./api/menuRoutes');
 
 router.use('/', viewRoutes);
 router.use('/api', apiRoutes);
 router.use('/users', userRoutes);
 router.use('/events', eventRoutes);
+router.use('/menu', menuRoutes);
 
 module.exports = router;
