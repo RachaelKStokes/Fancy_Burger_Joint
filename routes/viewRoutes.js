6abb48370c5f75dde67202cfbdb33396b8e17445
Zach <zach.Brewer.1995@gmail.com>
2024-02-12 20:47:30 -0500

    added reservation form

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,21 +2,23 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
     res.render('homepage', {
-      users,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+router.get('/menu', withAuth, async (req, res) => {
+  try {
+    res.render('menu', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).redirect('/login');
   }
 });
 
