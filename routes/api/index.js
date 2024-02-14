d59c0eac73c4a2fe83f90562442864b1de938c50
Sarahbryant411 <sarahbryant411@gmail.com>
2024-02-14 17:05:36 -0500

    too withAuth out of everything but viewRoutes.js- per anthony

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const eventRoutes = require('./eventRoutes');
 const menuRoutes = require('./menuRoutes');
+//const reservationRoutes = require('./reservationRoutes');
 
 
 router.use('/users', userRoutes);
 router.use('/events', eventRoutes);
 router.use('/menu', menuRoutes);
+// router.use('/reservations', reservationRoutes);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const { User } = require("../../models");
-const withAuth = require("../../utils/auth");
 
 // GET all users
-router.get("/", withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
 try {
     const userData = await User.findAll({
     attributes: { exclude: ["password"] },
